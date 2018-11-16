a39c6a2d24dd4b2924328226767b47caffd6afda
moohat <refixzcorp@gmail.com>
2018-11-16 14:50:00 +0700

    penambahan api login

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -3,11 +3,12 @@
 namespace App\Http\Controllers\Api;
 
 use DB;
-use Response;
+use Auth;
 use App\Models\User;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Http\Resources\UsersResource;
+use Illuminate\Support\Facades\Response;
 
 class UserController extends Controller
 {
@@ -18,5 +19,34 @@ class UserController extends Controller
     	//dd($users);
 
     	return UsersResource::collection($users);
-    }
+	}
+	
+	public function login(Request $request){
+
+		if( ! Auth::attempt(['email' => $request->email, 'password' => $request->password])){
+			
+			return Response::json([
+				'status'=>[
+					"code" =>401,
+					"description"=>"credential is wrong"
+				]
+				], 401);
+
+
+		}
+
+		$loggedUser = User::find(Auth::user()->id);
+
+
+		return(new UsersResource($loggedUser))
+		->additional([
+			'status'=>[
+				"code"=>200,
+				"description"=> "OK"
+			]
+		])->response()->setStatusCode(202);
+		
+	}
+
+	
 }
--- routes/api.php
+++ routes/api.php
@@ -20,3 +20,5 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
 
 
 Route::get('users','Api\UserController@users');
+
+Route::post('auth/login','Api\UserController@login');
